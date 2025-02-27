bb0b351888b7c426e524d27ab65bca4e79b0c131
Martin Askolin <80485419+martinaskolin@users.noreply.github.com>
2025-02-27 14:12:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pika
 import time
 
 time.sleep(30)
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq1"))
 channel = connection.channel()
 channel.queue_declare(queue='hello')
 channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
