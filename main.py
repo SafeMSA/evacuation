8d993f778bd4521dc3965abf2aa2f1b9c86d5861
martinaskolin <martinaskolin99@gmail.com>
2025-03-19 12:50:11 +0100

    askdaksd

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def connect_to_rabbitmq():
     while True:
         for host in RABBITMQ_HOST:
             try:
-                parameters = pika.ConnectionParameters(host=host, credentials=credentials)
+                parameters = pika.ConnectionParameters(host=host, credentials=credentials, blocked_connection_timeout=1)
                 connection = pika.BlockingConnection(parameters)
                 channel = connection.channel()
                 channel.exchange_declare(exchange='notifications', exchange_type='fanout', durable=True)
@@ -27,45 +27,50 @@ def connect_to_rabbitmq():
                 time.sleep(1)
 
 def start_server(host='0.0.0.0', port=9092):
+    
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.bind((host, port))
         server_socket.listen(5)
         print(f"Listening on {host}:{port}...")
         
         while True:
-            client_socket, client_address = server_socket.accept()
-            with client_socket:
-                print(f"Connection from {client_address}")
-                request = client_socket.recv(1024).decode('utf-8')
+            try:
+                client_socket, client_address = server_socket.accept()
+                with client_socket:
+                    print(f"Connection from {client_address}")
+                    request = client_socket.recv(1024).decode('utf-8')
 
-                # Try to parse the incoming request as JSON
-                if (len(request) > 10):
-                    # Build JSON response
-                    response_data = {
-                        "name": NAME,
-                        "code": 200
-                    }
-                    response_json = json.dumps(response_data, indent=2)
+                    # Try to parse the incoming request as JSON
+                    if (len(request) > 10):
+                        # Build JSON response
+                        response_data = {
+                            "name": NAME,
+                            "code": 200
+                        }
+                        response_json = json.dumps(response_data, indent=2)
 
-                    response = (
-                        "HTTP/1.1 200 OK\r\n"
-                        "Content-Type: application/json\r\n"
-                        f"Content-Length: {len(response_json)}\r\n"
-                        "\r\n"
-                        f"{response_json}"
-                    )
+                        response = (
+                            "HTTP/1.1 200 OK\r\n"
+                            "Content-Type: application/json\r\n"
+                            f"Content-Length: {len(response_json)}\r\n"
+                            "\r\n"
+                            f"{response_json}"
+                        )
 
-                    # Extract JSON body from HTTP request
-                    headers, body = request.split("\r\n\r\n", 1)
+                        # Extract JSON body from HTTP request
+                        headers, body = request.split("\r\n\r\n", 1)
 
-                    # Validate JSON
-                    json_data = json.loads(body)
+                        # Validate JSON
+                        json_data = json.loads(body)
 
-                    client_socket.sendall(response.encode('utf-8'))
-                    # Publish the message
-                    print("Sending out message")
-                    channel.basic_publish(exchange='notifications', routing_key='', body=json.dumps(json_data),
-                        properties=pika.BasicProperties(delivery_mode=2))  # Make message persistent
+                        client_socket.sendall(response.encode('utf-8'))
+                        # Publish the message
+                        print("Sending out message")
+                        channel.basic_publish(exchange='notifications', routing_key='', body=json.dumps(json_data),
+                            properties=pika.BasicProperties(delivery_mode=2))  # Make message persistent
+            except Exception as e:
+                print(e)
+                break
 
                 
 while True:
